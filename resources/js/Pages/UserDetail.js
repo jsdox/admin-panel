5bf4d1eb980a21754db493ef75d15517f32f75bb
jsucreate <jatindersingh@ucreate.co.in>
2021-05-27 12:46:09 +0530

    add user detail react file

---
+++ resources/js/Pages/UserDetail.js
@@ -0,0 +1,12 @@
+import Sidebar from '@/Layouts/Sidebar';
+import React from 'react';
+import { InertiaLink } from '@inertiajs/inertia-react';
+import { Link } from 'react-router-dom';
+
+export default function Users(props) {
+    return (
+        <Sidebar>
+           dsfdsf
+        </Sidebar>
+    );
+}
