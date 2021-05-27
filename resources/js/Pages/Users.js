0bef8645650a8da29d47d55946f7171beebeba0e
jsucreate <jatindersingh@ucreate.co.in>
2021-05-27 12:48:01 +0530

    add page for users

--- resources/js/Pages/Users.js
+++ resources/js/Pages/Users.js
@@ -1,26 +1,108 @@
-import Authenticated from '@/Layouts/Authenticated';
-import React from 'react';
+// import Authenticated from '@/Layouts/Authenticated';
+// import React from 'react';
 
-export default function Dashboard(props) {
-    const user_data = props.data.map((data) => 
-        <li key={data.id}>
-        {data.email}
-        </li>
-    );
+// export default function Dashboard(props) {
+//     const user_data = props.data.map((data) => 
+//         <li key={data.id}>
+//         {data.email}
+//         </li>
+//     );
+//     return (
+//         <Authenticated
+//             auth={props.auth}
+//             errors={props.errors}
+//             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+//         >
+//             <div className="py-12">
+//                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+//                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+//                         <div className="p-6 bg-white border-b border-gray-200">Users</div>
+//                         <ul>{user_data}</ul>
+//                     </div>
+//                 </div>
+//             </div>
+//         </Authenticated>
+//     );
+// }
+
+import Sidebar from '@/Layouts/Sidebar';
+import React from 'react';
+import { InertiaLink } from '@inertiajs/inertia-react';
+import { Link } from 'react-router-dom';
+
+export default function Users(props) {
+    const people = props.data;
     return (
-        <Authenticated
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">Users</div>
-                        <ul>{user_data}</ul>
+        <Sidebar>
+           <main className="flex-1 relative overflow-y-auto focus:outline-none">
+                <div className="py-6">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                    </div>
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                        {/* Replace with your content */}
+                        <div className="py-4">
+                            {/* <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" /> */}
+                            <div className="flex flex-col">
+                                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                        <table className="min-w-full divide-y divide-gray-200">
+                                        <thead className="bg-gray-50">
+                                            <tr>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                Name
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                Email
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                phone
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                            >
+                                                Gender
+                                            </th>
+                                            <th scope="col" className="relative px-6 py-3">
+                                                <span className="sr-only">Edit</span>
+                                            </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {people.map((person, personIdx) => (
+                                            <tr key={person.id} className={personIdx % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{person.name}</td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.email}</td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.phone_number}</td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.gender === 1 ? 'Male' : 'Female'}</td>
+                                                <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                    <a href={route('users')} className="text-indigo-600 hover:text-indigo-900"></a>    
+                                                    <InertiaLink method="get" href={route('users')+'/'+person.id}>View</InertiaLink>                                           
+                                                </td>
+                                            </tr>
+                                            ))}
+                                        </tbody>
+                                        </table>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* /End replace */}
                     </div>
                 </div>
-            </div>
-        </Authenticated>
+            </main>
+        </Sidebar>
     );
 }
