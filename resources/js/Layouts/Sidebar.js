4cce5c198fb3f224e27de1a2c1e73a9931ec33c4
jsucreate <jatindersingh@ucreate.co.in>
2021-06-09 10:22:17 +0530

    fix navigation

--- resources/js/Layouts/Sidebar.js
+++ resources/js/Layouts/Sidebar.js
@@ -16,12 +16,12 @@ import {
 import { SearchIcon } from '@heroicons/react/solid'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: InboxIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
+  { name: 'Users', href: '/users/', icon: HomeIcon, current: true },
+//   { name: 'Team', href: '#', icon: UsersIcon, current: false },
+//   { name: 'Projects', href: '#', icon: FolderIcon, current: false },
+//   { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+//   { name: 'Documents', href: '#', icon: InboxIcon, current: false },
+//   { name: 'Reports', href: '#', icon: ChartBarIcon, current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
